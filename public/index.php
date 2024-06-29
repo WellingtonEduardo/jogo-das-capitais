3c5191d11c0226fedda7e4504b16bb9ccc55c0be
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-06-29 13:48:47 -0300

    I finished the game

--- public/index.php
+++ public/index.php
@@ -9,11 +9,13 @@ $route = $_GET["route"] ?? "start";
 $script = match ($route) {
     "start" => "start" ,
     "game" => "game" ,
-    "gameOver" => "gameOver" ,
+    "gameover" => "gameover" ,
     default => "404"
 };
 
 
 $capitals = require __DIR__ . "/../data/capitals.php";
 
-var_dump($capitals);
+require_once __DIR__ . "/../scripts/header.php";
+require_once __DIR__ . "/../scripts/$script.php";
+require_once __DIR__ . "/../scripts/footer.php";
